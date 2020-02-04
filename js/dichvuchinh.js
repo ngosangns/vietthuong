184b81bc78e9b4633bbfebfa10b1a5194c0b8ecf
NGO SANG <46277436+ngosangns@users.noreply.github.com>
2020-02-05 02:46:42 +0700

    gần xong trang chủ

---
+++ js/dichvuchinh.js
@@ -0,0 +1,17 @@
+var dichvuchinh = [
+    {
+        name: "Tổ chức event - Sự kiện chuyên nghiệp",
+        content: "Nobis assentior an quo, quo docendi efficiendi ad. Mei sententiae definitiones no, ex postea laoreet ceteros eos, singulis definitiones conclusionemque no sea. Cu tale vidit cotidieque mei, ei nusquam ponderum vis. Feugait definiebas ei vel, intellegam comprehensam duo ad.",
+        image: "no-background.jpg",
+    },
+    {
+        name: "Đào tạo giáo viên, nhạc công",
+        content: "Nobis assentior an quo, quo docendi efficiendi ad. Mei sententiae definitiones no, ex postea laoreet ceteros eos, singulis definitiones conclusionemque no sea. Cu tale vidit cotidieque mei, ei nusquam ponderum vis. Feugait definiebas ei vel, intellegam comprehensam duo ad.",
+        image: "no-background.jpg",
+    },
+    {
+        name: "Viết sheet, phối nhạc",
+        content: "Nobis assentior an quo, quo docendi efficiendi ad. Mei sententiae definitiones no, ex postea laoreet ceteros eos, singulis definitiones conclusionemque no sea. Cu tale vidit cotidieque mei, ei nusquam ponderum vis. Feugait definiebas ei vel, intellegam comprehensam duo ad.",
+        image: "no-background.jpg",
+    },
+]
---
+++ js/sanpham.js
@@ -0,0 +1,22 @@
+var sanpham = [
+    {
+        name : "Piano",
+        image : "piano.jpg",
+    },
+    {
+        name : "Guitar",
+        image : "guitar.jpg",
+    },
+    {
+        name : "Ukulele",
+        image : "ukulele.jpg",
+    },
+    {
+        name : "Violin",
+        image : "violin.jpg",
+    },
+    {
+        name : "Organ",
+        image : "organ.jpg",
+    },
+]
